eec93e9f4940fade5845dd5786580eb219b2574b
Viking92 <kacper.oleksiak6@gmail.com>
2014-04-28 21:55:05 +0200

    Trochę komentarzy

--- model.h
+++ model.h
@@ -13,12 +13,51 @@ public:
     ~Model(void);
     void setView(MainWindow*);
     void setAI(AIModule*);
+
+    /*!
+     * \brief Zwraca informację czy dane pole można kliknąć.
+     * \return Informacja czy dane pole można kliknąć.
+     */
     bool isClickable(const Field&);
+
+    /*!
+     * \brief Zwraca zbiory pól, na które można wykonać ruch z danego pola.
+     * \return Para wektorów. Pierwszy zawiera pola, na które można się sklonować.
+     * Drugi zawiera pola, na które można przeskoczyć.
+     */
     std::pair<std::vector<Field>, std::vector<Field> > getAvailbleFields(const Field&);
+
+    /*!
+     * \brief Zwraca wskaźnik na tablicę.
+     * \return Wskaźnik na tablicę.
+     */
     char** getBoard(void) const;
+
+    /*!
+     * \brief Ustawia model w stan początkowy.
+     * \return Wskaźnik na tablicę.
+     */
     char** reset(void);
+
+    /*!
+     * \brief Dokonanie ruchu.
+     *
+     * Pierwszy parametr określa rodzaj ruchu (sklonowanie lub przeskok).
+     * Drugi parametr to para pól. Pierwsze to te, z którego wykonano ruch.
+     * Drugie to te, na które wykonano ruch.
+     */
     void action(char, const std::pair<Field, Field>&);
+
+    /*!
+     * \brief Zwraca ilość czerwonych.
+     * \return Ilość czerwonych.
+     */
     unsigned char getReds(void) const;
+
+    /*!
+     * \brief Zwraca ilość niebieskich.
+     * \return Ilość niebieskich.
+     */
     unsigned char getBlues(void) const;
 
     const static unsigned char N_ROWS = 9;
